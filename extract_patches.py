2da343c40b4c78fe1136a85f74c328abec4d800c
Ryan Visée <ryan.visee@mail.utoronto.ca>
2019-06-11 11:12:56 -0400

    Fixed extraction method to output 1D vector for labels containing 0 or 1

--- extract_patches.py
+++ extract_patches.py
@@ -9,32 +9,7 @@ import cv2
 import numpy as np
 from sklearn.feature_extraction import image
 
-#Preprocess the image for training
-def extract_patches(train_img, train_labels,num_patches,n_dim):
-    """
-    This function takes train_img and train_labels and splits it into num_patches
-    for training. Note that extract_patches_2d results in overlapping regions. 
-    
-    Input:
-        train_img, train_labels --> image name and corresponding labels to be split
-        num_patches --> int:
-            Total number of patches that train_img and train_labels will be split into
-        n_dim --> int:
-            Desired height/width of input images into network
-        
-    Returns:
-        img_patches,lab_patches:
-            num_patches of train_img and corresponding train_labels        
-    """
-    
-    
-    train_img = cv2.imread(train_img)
-    train_labels = cv2.imread(train_labels)
 
-    img_patches = image.extract_patches_2d(train_img, (n_dim, n_dim),max_patches=num_patches,random_state=1)
-    lab_patches = image.extract_patches_2d(train_labels, (n_dim, n_dim),max_patches=num_patches,random_state=1)
-
-    return img_patches,lab_patches
 
 def noOverlap_extract(train_img,train_labels,n_dim):
     """
@@ -71,4 +46,12 @@ def noOverlap_extract(train_img,train_labels,n_dim):
             img_patches.append(train_img[n_dim*j:n_dim*(1+j),n_dim*i:n_dim*(1+i)])
             lab_patches.append(train_labels[n_dim*j:n_dim*(1+j),n_dim*i:n_dim*(1+i)])
             
-    return img_patches,lab_patches
+    labels = []
+    for i in range(len(lab_patches)):
+        maxes = []
+        for j in range(n_dim):
+            for k in range(n_dim):
+                maxes.append(max(lab_patches[i][j][k]))
+        labels.append(maxes)
+        
+    return np.array(img_patches),np.array(labels)
