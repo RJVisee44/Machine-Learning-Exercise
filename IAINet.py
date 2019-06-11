8ca4ff3aee9a13fb2dbd6f6974373f016b9b1197
Ryan Visée <ryan.visee@mail.utoronto.ca>
2019-06-11 11:06:50 -0400

    Added dense layer for output

--- IAINet.py
+++ IAINet.py
@@ -6,7 +6,7 @@ Created on Mon Jun 10 09:36:19 2019
 """
 
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D,Conv2DTranspose
+from keras.layers import Conv2D, MaxPooling2D,Conv2DTranspose,Flatten,Dense
 from keras.utils import plot_model
 
 
@@ -19,13 +19,17 @@ def IAINet(input_shape):
     #Create model
     model = Sequential()
     
-    model.add(Conv2D(16,(3,3),activation="relu",input_shape=input_shape))
+    model.add(Conv2D(16,(3,3),input_shape=input_shape))
     model.add(Conv2D(16,(5,5),activation="relu"))
     model.add(MaxPooling2D())
     model.add(Conv2D(32,(3,3),activation="relu"))
     model.add(Conv2DTranspose(32,(2,2)))
     model.add(Conv2D(16,(3,3)))
-
+    
+    #Need fully connected layer for output 
+    model.add(Flatten())
+    model.add(Dense(input_shape[0]*input_shape[0], input_shape=(input_shape[0], ), activation='relu'))
+    
     plot_model(model, to_file='model.png')
 
     return model
